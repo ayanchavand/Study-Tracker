de749dd4c8500933eb99121f5a39838982129d10
Ayan Chavand <71554089+ayanchavand@users.noreply.github.com>
2021-11-14 12:41:10 +0000

    Added comments and basic stats

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from os import write
+#Import necessary modules
+import os 
 from win10toast import ToastNotifier
 import time
 import msvcrt
@@ -6,12 +7,18 @@ import csv
 import os.path
 import datetime
 
+
 def Countdown():
   currentTime = str(datetime.datetime.now().time())
+
   #Required inputs
   Tm = int(input("Enter time in Minutes:"))
   Ts = int(input("Enter time in Seconds:"))
+
+  #Initialize dataEntry list for csv entry
   dataEntry = [currentTime, currentTopic, "CountDown", Tm, Ts]
+
+  #Convert time in seconds 
   Tm *= 60
   Time = Tm + Ts 
 
@@ -23,53 +30,77 @@ def Countdown():
 
   #notification for the time up    
   toaster.show_toast("Alarm", "Time's up", icon_path ="Timer.ico")
+
+  #write the data in csv
   writer.writerow(dataEntry)
 
+
 def Timer():
+  Time = 0
   currentTime = str(datetime.datetime.now().time())
   print("Timer has started! Press Esc to stop.")
-  #Initialize time
-  Time = 0
+
   #Timer loop
   while True:
-    #End timer when pressed escape
-    if msvcrt.kbhit() and msvcrt.getch() == chr(27).encode():
-      #Calculate Hours and Minutes
-      if Time > 60:
-        Tm = str(Time // 60)
-        Ts = str(Time % 60)
-        print("Stopped after " +Tm+ " minutes and " +Ts+ " seconds.")
-        dataEntry = [currentTime, currentTopic, "Timer", Tm, Ts]
-        writer.writerow(dataEntry)
-        break
-      #else print the Seconds
-      else:
-        print("Stopped after " + str(Time) + " seconds.")
-        dataEntry = [currentTime, currentTopic, "Timer", "0", Time]
-        writer.writerow(dataEntry)
-        break
     Time += 1
     time.sleep(1)
 
+    #End timer when pressed escape
+    if msvcrt.kbhit() and msvcrt.getch() == chr(27).encode():
+      #Calculate Minutes and Seconds
+      if Time > 60:
+        #Convert seconds into minutes and seconds
+        Tm = str(Time // 60)
+        Ts = str(Time % 60)
 
+        print("Stopped after " +Tm+ " minutes and " +Ts+ " seconds.")
 
+        #Initialize dataEntry list for csv entry
+        dataEntry = [currentTime, currentTopic, "Timer", Tm, Ts]
+        #write the data in csv and break the loop
+        writer.writerow(dataEntry)
+        break
 
+      else:
+        print("Stopped after " + str(Time) + " seconds.")
 
+        #Initialize dataEntry list for csv entry
+        dataEntry = [currentTime, currentTopic, "Timer", "0", Time]
+        #write the data in csv and break the loop
+        writer.writerow(dataEntry)
+        break
+
+def Stats():
+  f = open("Study Report.csv", 'r')
+  reader = csv.DictReader(f)
+  totalTime = 0
+  for row in reader:
+    Time = int(row["Duration (sec)"])
+    totalTime += Time
+  print(totalTime)
+    
 
 #Initialize Toaster for notifications
 toaster = ToastNotifier()
 
-header = ["Time", "Assignment", "Mode", "Duration (min)", "Duration (sec)"]
-
+#Create the csv and add the header if the csv doesn't exists
 if not os.path.isfile('Study Report.csv'):
   f = open('Study Report.csv', 'w')
   writer = csv.writer(f)
+  header = ["Time", "Assignment", "Mode", "Duration (min)", "Duration (sec)"]
   writer.writerow(header)
 
+#else open in append mode
 else:
   f = open('Study Report.csv', 'a')
   writer = csv.writer(f)
-  
+
+mode = int(input("mode:"))
 currentTopic = str(input("Enter task:"))
 
-Countdown() 
+if mode == 0:
+  Stats()
+elif mode == 1:
+  Countdown()
+else:
+  Timer()
